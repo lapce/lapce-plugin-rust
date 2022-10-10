768bb44edf5ca3d777a5ba13ec8d2de10b80cdae
Jakub Panek <me@panekj.dev>
2022-10-10 11:41:30 +0200

    feat: add helper struct for envvar abstraction (#14)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-    path::PathBuf,
+    env,
     sync::{
         atomic::{AtomicU64, Ordering},
         Arc,
@@ -23,6 +23,42 @@ use serde_json::Value;
 use wasi_experimental_http::Response;
 
 pub static PLUGIN_RPC: Lazy<PluginServerRpcHandler> = Lazy::new(PluginServerRpcHandler::new);
+
+/// Helper struct abstracting environment variables
+/// names used in lapce to provide revelant host
+/// environment information, so plugin maintainers
+/// don't have to hardcode specific variable names
+pub struct VoltEnvironment {}
+
+impl VoltEnvironment {
+    /// Plugin location path encoded as Url
+    pub fn uri() -> Result<String, env::VarError> {
+        env::var("VOLT_URI")
+    }
+
+    /// Operating system name as provided by
+    /// std::env::consts::OS
+    pub fn operating_system() -> Result<String, env::VarError> {
+        env::var("VOLT_OS")
+    }
+
+    /// Processor architecture name as provided by
+    /// std::env::consts::ARCH
+    pub fn architecture() -> Result<String, env::VarError> {
+        env::var("VOLT_ARCH")
+    }
+
+    /// C library used on host detected by parsing ldd output
+    /// provided because of musl-based linux distros and distros
+    /// that need statically linked binaries due to how
+    /// linking works (e.g. nixOS)
+    /// Currently only 2 options are available: glibc | musl
+    /// This function will return empty string on non-linux
+    /// hosts
+    pub fn libc() -> Result<String, env::VarError> {
+        env::var("VOLT_LIBC")
+    }
+}
 
 #[allow(unused_variables)]
 pub trait LapcePlugin {
